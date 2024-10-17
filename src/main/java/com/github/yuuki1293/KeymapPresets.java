ea201ebf8b8f00dbfdc32dd8e65dfbde90f55909
yuuki1293 <yuuki1293@outlook.jp>
2024-10-18 08:54:12 +0900

    feat:
    get all bounded keymaps

--- src/main/java/com/github/yuuki1293/KeymapPresets.java
+++ src/main/java/com/github/yuuki1293/KeymapPresets.java
@@ -2,23 +2,54 @@ package com.github.yuuki1293;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
+import net.fabricmc.fabric.mixin.client.keybinding.KeyBindingAccessor;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.LiteralText;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class KeymapPresets implements ModInitializer {
-	public static final String MOD_ID = "keymappresets";
+    public static final String MOD_ID = "keymappresets";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    private static KeyBinding keyBinding;
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Hello Fabric world!");
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"key.examplemod.spook", // The translation key of the keybinding's name
+			InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+			GLFW.GLFW_KEY_R, // The keycode of the key
+			"category.examplemod.test" // The translation key of the keybinding's category.
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.wasPressed()) {
+                final GameOptions options = MinecraftClient.getInstance().options;
+
+                for (KeyBinding key : options.allKeys) {
+                    String message = String.format("%s, %s", key.getTranslationKey(), key.getBoundKeyTranslationKey());
+                    client.player.sendMessage(new LiteralText(message), false);
+                }
+            }
+        });
+
+
+    }
 }
