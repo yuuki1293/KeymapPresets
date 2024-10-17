5d948d2ffaab0e4804b6f584518e43a8418a41df
yuuki1293 <yuuki1293@outlook.jp>
2024-10-17 22:48:14 +0900

    first commit

---
+++ src/main/java/com/github/yuuki1293/KeymapPresets.java
@@ -0,0 +1,24 @@
+package com.github.yuuki1293;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KeymapPresets implements ModInitializer {
+	public static final String MOD_ID = "keymappresets";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/com/github/yuuki1293/KeymapPresetsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.github.yuuki1293;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KeymapPresetsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
