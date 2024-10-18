d43ea902d293bc0d5efc4498a3f233bdd392b134
yuuki1293 <yuuki1293@outlook.jp>
2024-10-19 00:09:44 +0900

    feat: command suggestion

--- src/main/java/com/github/yuuki1293/KeymapPresets.java
+++ src/main/java/com/github/yuuki1293/KeymapPresets.java
@@ -1,14 +1,17 @@
 package com.github.yuuki1293;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.command.CommandSource;
 import net.minecraft.text.LiteralText;
 import org.apache.commons.io.FilenameUtils;
 import org.lwjgl.glfw.GLFW;
@@ -23,7 +26,10 @@ import java.util.Optional;
 public class KeymapPresets implements ModInitializer {
     public static final String MOD_ID = "keymappresets";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final MinecraftClient client = MinecraftClient.getInstance();
+    public static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+    public static final SuggestionProvider<FabricClientCommandSource> SUGGESTION_PROVIDER = (context, builder) -> CommandSource.suggestMatching(
+        getPresets(), builder
+    );
 
     private static KeyBinding keyBindingSave;
 
@@ -47,16 +53,24 @@ public class KeymapPresets implements ModInitializer {
             ClientCommandManager.literal("keymap")
                 .then(ClientCommandManager.literal("save")
                     .then(ClientCommandManager.argument("name", StringArgumentType.string())
+                        .suggests(SUGGESTION_PROVIDER)
                         .executes(context -> {
                             final String presetName = StringArgumentType.getString(context, "name");
-                            saveKeymap(presetName);
+                            if (saveKeymap(presetName))
+                                context.getSource().sendError(new LiteralText("Keymap " + presetName + " can't save."));
+                            else
+                                context.getSource().sendFeedback(new LiteralText("Keymap " + presetName + " saved!"));
                             return 1;
                         })))
                 .then(ClientCommandManager.literal("load")
                     .then(ClientCommandManager.argument("name", StringArgumentType.string())
+                        .suggests(SUGGESTION_PROVIDER)
                         .executes(context -> {
                             final String presetName = StringArgumentType.getString(context, "name");
-                            loadKeymap(presetName);
+                            if (loadKeymap(presetName))
+                                context.getSource().sendError(new LiteralText("Keymap " + presetName + " can't load."));
+                            else
+                                context.getSource().sendFeedback(new LiteralText("Keymap " + presetName + " loaded!"));
                             return 1;
                         })))
                 .then(ClientCommandManager.literal("list")
@@ -68,8 +82,8 @@ public class KeymapPresets implements ModInitializer {
         );
     }
 
-    private void saveKeymap(String presetName) {
-        final File keymapDirectory = new File(client.runDirectory, MOD_ID);
+    private boolean saveKeymap(String presetName) {
+        final File keymapDirectory = new File(CLIENT.runDirectory, MOD_ID);
         final File presetFile = new File(keymapDirectory, presetName + ".txt");
 
         try {
@@ -81,20 +95,23 @@ public class KeymapPresets implements ModInitializer {
 
         } catch (IOException e) {
             LOGGER.error("Couldn't create preset file", e);
+            return true;
         }
 
         try (FileWriter fileWriter = new FileWriter(presetFile)) {
-            for (KeyBinding key : client.options.allKeys) {
+            for (KeyBinding key : CLIENT.options.allKeys) {
                 String keymap = String.format("%s:%s", key.getTranslationKey(), key.getBoundKeyTranslationKey());
                 fileWriter.append(keymap).append("\n");
             }
+            return false;
         } catch (IOException e) {
             LOGGER.error("Couldn't write preset file", e);
+            return true;
         }
     }
 
-    private void loadKeymap(String presetName) {
-        final File keymapDirectory = new File(client.runDirectory, MOD_ID);
+    private boolean loadKeymap(String presetName) {
+        final File keymapDirectory = new File(CLIENT.runDirectory, MOD_ID);
         final File presetFile = new File(keymapDirectory, presetName + ".txt");
 
         try (BufferedReader br = new BufferedReader(new FileReader(presetFile))) {
@@ -103,7 +120,7 @@ public class KeymapPresets implements ModInitializer {
                 String translationKey = split[0];
                 String keyName = split[1];
 
-                Optional<KeyBinding> keyBinding = Arrays.stream(client.options.allKeys)
+                Optional<KeyBinding> keyBinding = Arrays.stream(CLIENT.options.allKeys)
                     .filter(keyBinding_ -> keyBinding_.getTranslationKey().equals(translationKey))
                     .findFirst();
 
@@ -112,13 +129,15 @@ public class KeymapPresets implements ModInitializer {
             }
 
             KeyBinding.updateKeysByCode();
+            return false;
         } catch (Exception e) {
             LOGGER.info("Keymap file does not exist", e);
+            return true;
         }
     }
 
-    private String[] getPresets() {
-        final File keymapDirectory = new File(client.runDirectory, MOD_ID);
+    private static String[] getPresets() {
+        final File keymapDirectory = new File(CLIENT.runDirectory, MOD_ID);
 
         return Arrays.stream(Objects.requireNonNull(keymapDirectory.listFiles(new FilenameFilter() {
                 @Override
