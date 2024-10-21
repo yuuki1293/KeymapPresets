dcd3fd505afab5d443da09fcc48b86a7dadd28f3
yuuki1293 <yuuki1293@outlook.jp>
2024-10-21 21:49:57 +0900

    chore: ModInitializer to ClientModInitializer

--- src/main/java/com/github/yuuki1293/KeymapPresets.java
+++ src/main/java/com/github/yuuki1293/KeymapPresets.java
@@ -1,13 +1,13 @@
 package com.github.yuuki1293;
 
 import com.github.yuuki1293.command.KeymapPresetsCommand;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
 import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KeymapPresets implements ModInitializer {
+public class KeymapPresets implements ClientModInitializer {
     public static final String MOD_ID = "keymappresets";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static final MinecraftClient CLIENT = MinecraftClient.getInstance();
@@ -15,7 +15,7 @@ public class KeymapPresets implements ModInitializer {
     public static final int COLOR_LINK = 0x0000EE;
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         KeymapPresetsCommand.register();
     }
 }
