017d8d88827b7e85db00366e0e94791856bead4d
yuuki1293 <yuuki1293@outlook.jp>
2024-10-22 00:12:02 +0900

    update: add radial menu

--- src/main/java/com/github/yuuki1293/screen/KeymapPresetsMenuScreen.java
+++ src/main/java/com/github/yuuki1293/screen/KeymapPresetsMenuScreen.java
@@ -1,13 +1,12 @@
 package com.github.yuuki1293.screen;
 
 import com.github.yuuki1293.IOLogic;
+import com.github.yuuki1293.KeymapPresets;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.LiteralText;
 
 public class KeymapPresetsMenuScreen extends Screen {
-    private static final int radius = 500; // WIP
-
     public KeymapPresetsMenuScreen() {
         super(LiteralText.EMPTY);
     }
@@ -18,15 +17,34 @@ public class KeymapPresetsMenuScreen extends Screen {
 
         final var x0 = this.width / 2;
         final var y0 = this.height / 2;
+        final double radius = this.height / 3.0;
 
         final var presets = IOLogic.getPresets();
         final var len = presets.length;
 
         for (int i = 0; i < len; i++) {
-            final int x = x0 + (int)Math.cos(2 * Math.PI / len * i) * radius;
-            final int y = y0 + (int)Math.sin(2 * Math.PI / len * i) * radius;
+            final int x = x0 + (int) (Math.cos(2 * Math.PI / (double)len * (double)i - Math.PI / 2.0) * radius);
+            final int y = y0 + (int) (Math.sin(2 * Math.PI / (double)len * (double)i - Math.PI / 2.0) * radius);
+            final var presetName = presets[i];
 
-            this.addSelectableChild(new ButtonWidget(x, y, 50, 50, new LiteralText(presets[i]), null));
+            this.addDrawableChild(new ButtonWidget(x - 25, y - 10, 50, 20, new LiteralText(presetName), button -> {
+                final var player = KeymapPresets.CLIENT.player;
+                if (player == null) {
+                    this.close();
+                    return;
+                }
+
+                if (IOLogic.loadKeymap(presetName))
+                    player.sendMessage(new LiteralText("Failed to load Keymap " + presetName), true);
+                else
+                    player.sendMessage(new LiteralText("Keymap " + presetName + " loaded!"), true);
+                this.close();
+            }));
         }
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
     }
 }
