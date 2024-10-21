f840c47d28fa34b464b4a17eb00a42a83e520d79
yuuki1293 <yuuki1293@outlook.jp>
2024-10-21 21:37:44 +0900

    chore: remove unused statement

--- src/main/java/com/github/yuuki1293/KeymapPresets.java
+++ src/main/java/com/github/yuuki1293/KeymapPresets.java
@@ -4,7 +4,6 @@ import com.github.yuuki1293.command.KeymapPresetsCommand;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,8 +13,6 @@ public class KeymapPresets implements ModInitializer {
     public static final MinecraftClient CLIENT = MinecraftClient.getInstance();
     public static final String URL_ISSUE = "https://github.com/yuuki1293/KeymapPresets/issues";
     public static final int COLOR_LINK = 0x0000EE;
-
-    private static KeyBinding keyBindingSave;
 
     @Override
     public void onInitialize() {
