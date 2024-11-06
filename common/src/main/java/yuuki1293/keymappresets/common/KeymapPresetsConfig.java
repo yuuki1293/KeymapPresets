f780c5bf8384269aa3cd6e135d60d763b8842db5
yuuki1293 <yuuki1293@outlook.jp>
2024-11-06 21:35:53 +0900

    コメントは使えないよ

--- common/src/main/java/yuuki1293/keymappresets/common/KeymapPresetsConfig.java
+++ common/src/main/java/yuuki1293/keymappresets/common/KeymapPresetsConfig.java
@@ -2,13 +2,11 @@ package yuuki1293.keymappresets.common;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 import static yuuki1293.keymappresets.common.Common.MOD_ID;
 
 @Config(name = MOD_ID)
 public class KeymapPresetsConfig implements ConfigData {
-    @Comment("This is deprecated. Please change it on the GUI.")
     public String selectedPreset = "";
 
     public SortType sortType = SortType.CREATED;
