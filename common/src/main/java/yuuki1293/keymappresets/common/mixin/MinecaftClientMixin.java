536c28bbaf305b360bdfe816bcfae228747d176c
yuuki1293 <yuuki1293@outlook.jp>
2025-05-27 17:27:30 +0900

    feat: prevent changing hotbar

--- common/src/main/java/yuuki1293/keymappresets/common/mixin/MinecaftClientMixin.java
+++ common/src/main/java/yuuki1293/keymappresets/common/mixin/MinecaftClientMixin.java
@@ -1,5 +1,8 @@
 package yuuki1293.keymappresets.common.mixin;
 
+import net.minecraft.client.option.KeyBinding;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import yuuki1293.keymappresets.common.Common;
 import yuuki1293.keymappresets.common.IOLogic;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,5 +17,15 @@ public class MinecaftClientMixin {
     @Inject(method = "<init>", at = @At("RETURN"))
     private void init(CallbackInfo ci) {
         IOLogic.load(CONFIG.get().selectedPreset);
+    }
+
+    @Redirect(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;wasPressed()Z", ordinal = 2))
+    private boolean wasPressed(KeyBinding instance) {
+        if(Common.wasPressed) {
+            instance.wasPressed();
+            return false;
+        } else {
+            return instance.wasPressed();
+        }
     }
 }
